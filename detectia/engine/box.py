ecb8a5d270c4a9b2d87540ca68e806d2343bfd72
Kartik Sharma <kartik4949@gmail.com>
2021-02-05 22:36:42 +0530

    Added inital setup for box encoder

--- detectia/engine/box.py
+++ detectia/engine/box.py
@@ -1,14 +1,103 @@
+import functools
 import tensorflow as tf
+import numpy as np
+from utils import compute_iou_boxes
 
 
-class Anchor:
+class BoxEncoder:
     def __init__(self, config):
         self.config = config
+        self.anchors = np.asarray(
+            [[10, 20], [30, 40], [50, 60], [200, 100]], np.float32)
+        self.input_image_shape = (416, 416)
+        self.grids = [13, 26]
+        self.num_anchors = 2
+        self.num_scale_level = 2
 
-    def _generate_anchors(
-        self,
-    ):
-        ...
+    def _best_anchors(self, boxes, anchors):
+        """_best_anchors.
 
+        Args:
+            boxes   : pseudo bounding boxes (N, 4).
+            anchors : pseudo anchor boxes   (A, 4).
+
+        Returns:
+            Intersection over union matrix  (N, A).
+        """
+
+        def _compute_iou_along_boxes(box):
+            nonlocal anchors
+            _compute_iou = functools.partial(compute_iou_boxes, b2=box)
+            return tf.map_fn(_compute_iou, anchors)
+        return tf.map_fn(_compute_iou_along_boxes, boxes)
+
+    def _assign_grid(self, box, grid):
+        return tf.math.floor(box[:, 0] * grid), tf.math.floor(box[:, 1] * grid)
+
+    @tf.function
     def compute_targets(self, boxes, class_ids):
-        ...
+
+        # assert num of anchors are compatible with num_scales.
+        # anchor_ratio = self.config.anchors % self.config.num_scales
+        anchor_ratio = 0
+        assert anchor_ratio == 0, f'Each feature scale should have same num anchors.'
+
+        # calculate centroid (cx,cy) and width,height of bboxes w.r.t image.
+        bb_xy = (boxes[:, 0:2] + boxes[:, 2:4]) / 2
+        bb_wh = boxes[:, 2:4] - boxes[:, 0:2]
+
+        # normalize i.e 0-1 range.
+        normalized_boxes_top = boxes[:, 0:2] / self.input_image_shape
+        normalized_boxes_bottom = boxes[:, 2:4] / self.input_image_shape
+        normalized_boxes = tf.concat(
+            [normalized_boxes_top, normalized_boxes_bottom], axis=-1)
+
+        # convert (wh) to (0,0,w,h) points
+        pseudo_bboxes = tf.concat([tf.zeros(shape=tf.shape(bb_wh)), bb_wh], axis=-1)
+        pseudo_anchor_boxes = tf.concat(
+            [tf.zeros(shape=tf.shape(self.anchors)), self.anchors], axis=-1)
+
+        # get the iou matrix for anchors.
+        _best_anchors = self._best_anchors(pseudo_bboxes, pseudo_anchor_boxes)
+
+        # top iou anchor id.
+        _best_anchors_ids = tf.argmax(_best_anchors, axis=-1)
+        _best_anchors_ids = tf.cast(_best_anchors_ids, tf.int32)
+
+        targets = []
+        for i in range(self.num_scale_level):
+            grid = self.grids[i]
+            lower_bound = tf.cast(tf.greater(
+                i * self.num_anchors - 1, _best_anchors_ids), dtype=tf.int32)
+            upper_bound = tf.cast(tf.greater(
+                _best_anchors_ids, i * self.num_anchors + self.num_anchors), dtype=tf.int32)
+            anchors_level_ids = tf.math.logical_not(
+                tf.cast(lower_bound + upper_bound, tf.bool))
+
+            best_boxes_lvl = tf.boolean_mask(normalized_boxes, anchors_level_ids)
+            best_boxes_lvl = tf.cast(best_boxes_lvl, tf.float32)
+
+            best_anchors_ids_lvl = tf.boolean_mask(_best_anchors_ids, anchors_level_ids)
+
+            gi, gj = self._assign_grid(best_boxes_lvl, grid)
+
+            target_lvl = tf.zeros(
+                shape=(grid, grid, self.num_anchors, 4), dtype=tf.float32)
+
+            idx = tf.stack([tf.cast(gi, tf.int32), tf.cast(gj, tf.int32),
+                            tf.cast(best_anchors_ids_lvl, tf.int32)])
+
+            idx = tf.transpose(idx)
+            target_lvl = tf.tensor_scatter_nd_update(
+                target_lvl, [idx % self.num_anchors], [best_boxes_lvl])
+            targets.append(target_lvl)
+        return targets
+
+
+if __name__ == "__main__":
+    config = None
+    boxes = [[200, 100, 300, 200], [10, 20, 30, 40], [28, 38, 40, 50]]
+    boxes = np.asarray(boxes, np.float32)
+    x = BoxEncoder(config)
+    x = x.compute_targets(boxes, None)
+    breakpoint()
--- detectia/engine/utils.py
+++ detectia/engine/utils.py
@@ -3,26 +3,24 @@ import tensorflow as tf
 
 def compute_iou_boxes(b1, b2):
     # determine the coordinates of the intersection rectangle
-    x_left = max(b1[0], b2[0])
-    y_top = max(b1[1], b2[1])
-    x_right = min(b1[2], b2[3])
-    y_bottom = min(b1[3], b2[3])
+    x_left = tf.math.maximum(b1[0], b2[0])
+    y_top = tf.math.maximum(b1[1], b2[1])
+    x_right = tf.math.minimum(b1[2], b2[3])
+    y_bottom = tf.math.minimum(b1[3], b2[3])
 
     if x_right < x_left or y_bottom < y_top:
         return 0.0
 
-    # The intersection of two axis-aligned bounding boxes is always an
+    # the intersection of two axis-aligned bounding boxes is always an
     # axis-aligned bounding box
     intersection_area = (x_right - x_left) * (y_bottom - y_top)
 
-    # compute the area of both AABBs
+    # compute the area of both aabbs
     bb1_area = (b1[2] - b1[0]) * (b1[3] - b1[1])
     bb2_area = (b2[2] - b2[0]) * (b2[3] - b2[1])
 
     # compute the intersection over union by taking the intersection
     # area and dividing it by the sum of prediction + ground-truth
     # areas - the interesection area
-    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
-    assert iou >= 0.0
-    assert iou <= 1.0
+    iou = intersection_area / (bb1_area + bb2_area - intersection_area)
     return iou
